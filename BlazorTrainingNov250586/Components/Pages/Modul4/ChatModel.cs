9d7cf1174c666038b237a3ef194c90d0c47e1a98
Hannes Preishuber <hannesp@ppedv.de>
2025-11-05 13:08:06 +0100

    minicaht mit UI Notification action aus Viewmodel

---
+++ BlazorTrainingNov250586/Components/Pages/Modul4/ChatModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorTrainingNov250586.Components.Pages.Modul4
+{
+    public class ChatModel
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ BlazorTrainingNov250586/Components/Pages/Modul4/ChatVM.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorTrainingNov250586.Components.Pages.Modul4
+{
+    public class ChatVM
+    {
+        public List<ChatModel> Liste { get; set; } = new();
+        public event Action OnAdd;
+        public void AddMessage(string msg)
+        {
+            Liste.Add(new ChatModel() { Message = msg });
+            OnAdd?.Invoke();
+        }
+    }
+}
