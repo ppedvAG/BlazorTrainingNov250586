1d411f4bbb3d5ecce8e51f636b8a6fe8265ce831
Hannes Preishuber <hannesp@ppedv.de>
2025-11-05 12:40:48 +0100

    Todo Rest Service als durchsuchbare Liste

---
+++ BlazorTrainingNov250586/Components/Pages/Modul4/ToDo.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorTrainingNov250586.Components.Pages.Modul4
+{
+    public class ToDos
+    {
+  
+        public Todo[] todos { get; set; }
+        public int total { get; set; }
+        public int skip { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class Todo
+    {
+        public int id { get; set; }
+        public string todo { get; set; }
+        public bool completed { get; set; }
+        public int userId { get; set; }
+    }
+
+}
---
+++ BlazorTrainingNov250586/Components/Pages/Modul4/ToDoVM.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorTrainingNov250586.Components.Pages.Modul4
+{
+    public class ToDoVM
+    {
+        public List<Todo> ToDoList { get; set; } = new List<Todo>();
+        public ToDoVM(IHttpClientFactory _http)
+        {
+            var client = _http.CreateClient();
+            var response = client.GetFromJsonAsync<ToDos>("https://dummyjson.com/todos?limit=300").Result;
+            ToDoList = response.todos.ToList();
+
+        }
+    }
+}
